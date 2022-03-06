05aab4c6af26f5f87231dfb82a40d9c9dddca77e
VincentGichohi <vincentegichohi@gmail.com>
2022-03-06 21:38:00 +0300

    Added variables to script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var Male = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+var Female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+
+var centuryDigits = CC
+var yearDigits = YY
+var month = MM
+var day = DD
